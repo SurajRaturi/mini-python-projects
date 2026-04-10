c8a9947726ad65feba17606daa73dba92c0545ce
SurajRaturi <surajraturi.5122006@gmail.com>
2026-04-10 15:23:13 +0530

    added a new mini project

---
+++ Smart dice roller/Readme.md
@@ -0,0 +1,15 @@
+<u><h1>Smart Dice Roller</h1></u><br>
+<b>Rules</b><br>
+•	Dice would roll randomly on choosing Y for 10seconds.<br>
+•	For each outcome as 1 and 6 ,player get the bonus +1sec added into total playing time.<br>
+•	If someone chooses N instead of Y , then the player simply come out of loop(game).<br>
+<b>Module Used</b><br>
+•	Random module<br>
+•	Datetime module<br>
+<b>Future enhancements</b>
+•	Adding file handling concept could helps to preserve the highest record to compete next time.<br>
+•	Edge cases should be resolved.<br>
+
+
+
+
---
+++ Smart dice roller/smart_dice_roller.py
@@ -0,0 +1,22 @@
+import random as r
+from datetime import datetime,timedelta
+score=0
+start_time=datetime.now()
+end_time=(start_time + timedelta(seconds=5))#game run for 10seconds after start
+print("----🎲 SMART DICE ROLLER 🎲----")
+print("Rules 📝 \n1.Game run for 10 seconds.\n2.Dice would roll randomly each time you choose Y.\n3.When outcomes are 1 or 6 then playing time increase by +1sec.")
+while datetime.now()<end_time:
+    user=input("Do you want to roll dice (Y/N) ? : ")
+    if user.lower()=="y":
+        dice_roll=r.randint(1,6)
+        print(f"{dice_roll} came!")
+        if dice_roll==1 or dice_roll==6:#bonous 1seconds play for dice no. 1 or 6
+            end_time=end_time + timedelta(seconds=1)
+            score+=dice_roll
+            print("+1sec 🕐\n")
+        else:
+            score+=dice_roll
+    else:
+        break
+print("\n")
+print(f"Total score : {score}\nTotal time taken in seconds : {(end_time-start_time).seconds}\nGame start time : {start_time.time()}\nGame end time : {end_time.time()}")
